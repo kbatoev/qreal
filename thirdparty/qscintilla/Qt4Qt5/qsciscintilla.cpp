f904dd59aace439e41d66170692189db9d848f31
Konstantin Batoev <kbatoev@mail.ru>
2017-10-21 02:05:10 +0300

    AutoComplete is handled appropriately

--- thirdparty/qscintilla/Qt4Qt5/qsciscintilla.cpp
+++ thirdparty/qscintilla/Qt4Qt5/qsciscintilla.cpp
@@ -551,8 +551,10 @@ QStringList QsciScintilla::apiContext(int pos, int &context_start,
 
     // A valid sequence always starts with a word and so should be expecting a
     // separator.
-    if (expecting != Separator)
-        words.clear();
+	//if (expecting != Separator)
+	//    words.clear();
+	if (words.count() == 1 && words.last().isEmpty())
+		words.clear();
 
     context_start = good_pos;
 
